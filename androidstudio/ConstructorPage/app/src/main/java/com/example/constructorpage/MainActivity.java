107750283bca3555577e8e1fde6d96ee1449f4dc
dinesh oswal <dineshoswal333@gmail.com>
2019-09-07 12:47:18 +0530

    Construct page task

---
+++ androidstudio/ConstructorPage/app/src/main/java/com/example/constructorpage/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.constructorpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       construct c=new construct();
+       c.construct1();
+    }
+}
---
+++ androidstudio/ConstructorPage/app/src/main/java/com/example/constructorpage/construct.java
@@ -0,0 +1,10 @@
+package com.example.constructorpage;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class construct {
+    public void construct1() {
+        System.out.println("Viratian for life");
+    }
+}
