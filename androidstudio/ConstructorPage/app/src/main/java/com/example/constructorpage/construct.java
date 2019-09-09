cb2ed7442437361878551c869aee83d3b31f4618
dinesh oswal <dineshoswal333@gmail.com>
2019-09-09 18:25:33 +0530

    on click toast message using constructor

--- androidstudio/ConstructorPage/app/src/main/java/com/example/constructorpage/construct.java
+++ androidstudio/ConstructorPage/app/src/main/java/com/example/constructorpage/construct.java
@@ -1,11 +1,20 @@
 package com.example.constructorpage;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.widget.Toast;
 
-public class construct extends MainActivity {
-    public void construct()
-    {
-        System.out.println("Viratian for life");
+import androidx.appcompat.app.AppCompatActivity;
+
+public class construct extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
-}
+      public void   construct(Context applicationContext)
+    {
+     Toast.makeText(applicationContext, "Dinesh Oswal is Viratian for life", Toast.LENGTH_LONG).show();
+    }
+ }
+
