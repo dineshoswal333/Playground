a301cb0e76ed8dfb383a1f16eb137470dc0b900f
dinesh oswal <dineshoswal333@gmail.com>
2019-09-09 18:25:33 +0530

    on click toast message using constructor and  setting permissions

--- androidstudio/ConstructorPage/app/src/main/java/com/example/constructorpage/MainActivity.java
+++ androidstudio/ConstructorPage/app/src/main/java/com/example/constructorpage/MainActivity.java
@@ -3,15 +3,27 @@ package com.example.constructorpage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity  {
+Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       button=findViewById(R.id.button);
+       button.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               construct c=new construct();
+               c.construct(getApplicationContext());
 
-       construct c=new construct();
-       c.construct();
+
+           }
+       });
+
     }
+
 }
