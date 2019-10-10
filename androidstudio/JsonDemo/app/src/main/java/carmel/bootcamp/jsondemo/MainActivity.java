ae5a9b2b336e15a4f51a795393ff390e132302dc
dinesh oswal <dineshoswal333@gmail.com>
2019-10-10 14:21:51 +0530

    json demo

---
+++ androidstudio/JsonDemo/app/src/main/java/carmel/bootcamp/jsondemo/MainActivity.java
@@ -0,0 +1,47 @@
+package carmel.bootcamp.jsondemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView output = (TextView) findViewById(R.id.textView);
+
+        String strJson="{ \"Employee\" :[{\"id\":\"101\",\"name\":\"Dinesh Suresh Oswal\",\"salary\":\"50000\"},{\"id\":\"102\",\"name\":\"Virat Kohli\",\"salary\":\"60000\"}] }";
+
+        String data = "";
+        try {
+            // Create the root JSONObject from the JSON string.
+            JSONObject  jsonRootObject = new JSONObject(strJson);
+
+            //Get the instance of JSONArray that contains JSONObjects
+            JSONArray jsonArray = jsonRootObject.optJSONArray("Employee");
+
+            //Iterate the jsonArray and print the info of JSONObjects
+            for(int i=0; i < jsonArray.length(); i++){
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                int id = Integer.parseInt(jsonObject.optString("id").toString());
+                String name = jsonObject.optString("name").toString();
+                float salary = Float.parseFloat(jsonObject.optString("salary").toString());
+
+                data += "Node"+i+" : \n id= "+ id +" \n Name= "+ name +" \n Salary= "+ salary +" \n ";
+            }
+            output.setText(data);
+        } catch (JSONException e) {e.printStackTrace();}
+
+
+    }
+}
